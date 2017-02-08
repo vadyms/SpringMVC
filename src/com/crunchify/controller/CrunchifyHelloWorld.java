4caf9720220023a11b34b4af6aa48d7050963b9b
vadymsn <vadym1981@gmail.com>
2017-02-08 17:10:09 +0200

    Added logger

--- src/com/crunchify/controller/CrunchifyHelloWorld.java
+++ src/com/crunchify/controller/CrunchifyHelloWorld.java
@@ -1,5 +1,7 @@
 package com.crunchify.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
@@ -11,10 +13,12 @@ import org.springframework.web.servlet.ModelAndView;
  
 @Controller
 public class CrunchifyHelloWorld {
+	
+	private static final Logger logger = LoggerFactory.getLogger(CrunchifyHelloWorld.class);
  
 	@RequestMapping("/welcome")
 	public ModelAndView helloWorld() {
- 
+		logger.info("controller welcome");
 		String message = "<br><div style='text-align:center;'>"
 				+ "<h3>********** Hello World, Spring MVC Tutorial</h3>This message is coming from CrunchifyHelloWorld.java **********</div><br><br>";
 		return new ModelAndView("welcome", "message", message);
